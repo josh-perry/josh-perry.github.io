72a56661c89e8a6d877947b17dae18d00bcc672d
Josh Perry <joshperry@outlook.com>
2025-07-12 12:11:46 +0100

    add duck

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,3 +1,10 @@
+import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+
 export default function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("bundle.css")
+    eleventyConfig.addPassthroughCopy('bundle.css')
+    eleventyConfig.addPassthroughCopy("assets/")
+
+    eleventyConfig.addFilter("dateDisplay", (dateObj) => {
+        return `${dateObj.getDate()} ${dateObj.toLocaleString('en', { month: 'long' })} ${dateObj.getFullYear()}`
+    })
 }
